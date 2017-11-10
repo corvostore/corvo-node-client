a10087bf0da3526f3b7ea7bd3b4abb45011b9c89
Peter Reznick <preznick@gmail.com>
2017-11-10 16:32:42 -0500

    add client and testing functionality

--- corvo_node_client.js
+++ corvo_node_client.js
@@ -5,31 +5,38 @@ class CorvoNodeClient {
     this.client = new Net.Socket();
     this.client.connect(6379, '127.0.0.1', this.handleConnection.bind(this));
   }
+  resolveOnData() {
+    return new Promise(resolve => {
+      this.client.on('data', function(data) {
+        resolve(data);
+      });
+    });
+  }
+
+  writeToServer(message) {
+    return new Promise(resolve => {
+      this.client.write(message, function(data) {
+        resolve(1);
+      });
+    });
+  }
 
   handleConnection(conn) {
-
     this.client.setEncoding("utf-8");
-
-    // this.client.on('data', function(data) {
-    //   console.log('Received: ' + data);
-    //   // client.destroy(); // kill client after server's response
-    // });
 
     this.client.on('close', function() {
       console.log('Connection closed');
     });
   }
 
-  set(key, val, callback) {
-    // create new promise, execute callback when data
-    const p = Promise.resolve(this.client.write("*2\r\n$3\r\nGET\r\n$1\r\nk\r\n"));
+  async set(key, val) {
+    const message = "*3\r\n$3\r\nSET\r\n$1\r\nk\r\n$3\r\nabc\r\n";
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
 
-    p.then(function(data) {
-      console.log(data);
-    });
+    this.client.destroy();
+    return returnVal;
   }
-
-
 }
 
 export default CorvoNodeClient;
--- spec/app_spec.js
+++ spec/app_spec.js
@@ -1,12 +1,20 @@
 import CorvoNodeClient from '../corvo_node_client';
 
 describe("first test", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+  //
+  // beforeEach((done) => {
+  //   client.writeToServer();
+  //   client.client.on('data', function(data) {
+  //     console.log("In testing callback");
+  //     response = data;
+  //     // done();
+  //   });
+  // });
+
   it("write test for client set command", () => {
-
-    const client = new CorvoNodeClient();
-
-    client.set("string key", "string val", function(response) {
-      console.log(response);
+    client.set(null, null).then((response) => {
       expect(response).toBe("+OK\r\n");
     });
   });
