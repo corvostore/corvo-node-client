dc8415db144e257dbedcd0397ce97abb960f0543
Peter Reznick <preznick@gmail.com>
2017-11-11 14:01:30 -0500

    add methods covering middle 11 store commands

--- corvo_node_client.js
+++ corvo_node_client.js
@@ -40,6 +40,95 @@ class CorvoNodeClient {
     return returnVal;
   }
 
+  async del(keys) {
+    const message = Encoder.encode("DEL", keys);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async lpush(...keys) {
+    const message = Encoder.encode("LPUSH", ...keys);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async hset(keys) {
+    const message = Encoder.encode("HSET", keys);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async lindex(key, index) {
+    index = index.toString();
+    const message = Encoder.encode("LINDEX", key, index);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async lrem(key, count, value) {
+    const message = Encoder.encode("LREM", key, index);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  };
+
+  async llen(key, count, value) {
+    const message = Encoder.encode("LREM", key, index);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  };
+
+  async linsert(key, flag, pivot, value) {
+    const message = Encoder.encode("LINSERT", key, flag, pivot, value);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async rpush(...keys) {
+    const message = Encoder.encode("RPUSH", ...keys);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async lpop(key) {
+    const message = Encoder.encode("LPOP", key);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async rpop(key) {
+    const message = Encoder.encode("RPOP", key);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async lset(key, index, val) {
+    const message = Encoder.encode("LSET", key, index, val);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  };
+
   destroyClient() {
     this.client.destroy();
   }
--- spec/app_spec.js
+++ spec/app_spec.js
@@ -18,4 +18,199 @@ describe("first test", () => {
       expect(response).toBe("+OK\r\n");
     });
   });
+
+  client.destroyClient();
+});
+
+describe("del", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("deletes a single key and returns 1", () => {
+    client.set("myKEY", "myval");
+    client.del("myKEY").then((response) => {
+      expect(response).toBe(":1\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("del", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("deletes multiple keys and returns the number of deletions", () => {
+    client.set("mykey1", "myval");
+    client.set("mykey2", "anotherVal");
+    client.del("mykey1", "mykey2").then((response) => {
+      expect(response).toBe(":2\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("hset", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("creates a hash, sets field and val, and returns 1", () => {
+    client.hset("my_key", "myField", "myVal").then((response) => {
+      expect(response).toBe(":1\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("lpush", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("creates a new list and adds a value", () => {
+    client.lpush("somekey", "myval").then((response) => {
+      expect(response).toBe(":1\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("lpush multiple", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("creates a list and adds multiple values", () => {
+    client.lpush("ZKEY", "myval", "sdfa", "dasfasd").then((response) => {
+      expect(response).toBe(":3\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("lindex", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("returns val at given index in list", () => {
+    client.lpush("zzz", "a", "b", "c");
+    client.lindex("asdfcd", 1).then((response) => {
+      expect(response).toBe("+b\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("lrem", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("returns number of removed elements", () => {
+    client.lpush("myList", "hello", "foo", "hello", "hello");
+    client.lrem("mylist", -2, "hello").then((response) => {
+      expect(response).toBe(":2\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("llen", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("returns the length of the list", () => {
+    client.lpush("myOtherList", "hello", "foo", "hello", "hello");
+    client.llen("myOtherlist").then((response) => {
+      expect(response).toBe(":4\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("linsert", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("the length of the list after the insert operation", () => {
+    client.lpush("myOtherOtherList", "hello", "foo", "hello", "hello");
+    client.linsert("myOtherOtherList", "AFTER", "foo", "newVal");
+    client.llen("myOtherOtherlist").then((response) => {
+      expect(response).toBe(":5\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("rpush", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("creates a new list and adds a value returning the length of the list", () => {
+    client.lpush("someNEWkey", "myval").then((response) => {
+      expect(response).toBe(":1\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("rpush multiple", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("creates a list and adds multiple values", () => {
+    client.lpush("XYZKEY", "myval", "sdfa", "dasfasd").then((response) => {
+      expect(response).toBe(":3\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("lpop", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("pops the left value off a list and returns it", () => {
+    client.lpush("zzzzbbbb", "myval", "sdfa", "dasfasd");
+    client.lpop("zzzzbbbb").then((response) => {
+      expect(response).toBe("+dasfasd\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("rpop", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("pops the right value off a list and returns it", () => {
+    client.lpush("zzb", "myval", "sdfa", "dasfasd");
+    client.lpop("zzb").then((response) => {
+      expect(response).toBe("+myval\r\n");
+    });
+  });
+
+  client.destroyClient();
+});
+
+describe("lset", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
+
+  it("returns OK if a value is successfully set", () => {
+    client.rpush("ZZZZZZZList", "aVAL!");
+    client.lset("ZZZZZZZList", 0, "differentVal").then((response) => {
+      expect(response).toBe("+OK\r\n");
+    });
+  });
+
+  client.destroyClient();
 });
