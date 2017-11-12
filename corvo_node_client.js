5c317dd4fb8af538cebdb7fe9f8c0e1c19841202
Sundaram Vedala <svedala23@gmail.com>
2017-11-12 05:39:35 -0500

    Add hash commands to corvo client

--- corvo_node_client.js
+++ corvo_node_client.js
@@ -40,6 +40,103 @@ class CorvoNodeClient {
     return returnVal;
   }
 
+  async hvals(key) {
+    const message = Encoder.encode("HVALS", key);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async hstrlen(key, field) {
+    const message = Encoder.encode("HSTRLEN", key, field);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async hmset(key, field, value, ...moreParams) {
+    const message = Encoder.encode("HMSET", key, field, value, ...moreParams);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async hdel(key, field, ...moreFields) {
+    const message = Encoder.encode("HDEL", key, field, ...moreFields);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async hget(key, field) {
+    const message = Encoder.encode("HGET", key, field);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async hgetall(key) {
+    const message = Encoder.encode("HGETALL", key);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async hlen(key) {
+    const message = Encoder.encode("HLEN", key);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async hsetnx(key, field, value) {
+    const message = Encoder.encode("HSETNX", key, field, value);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async hmget(key, field, ...moreFields) {
+    const message = Encoder.encode("HMGET", key, field, ...moreFields);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async hincrby(key, field, increment) {
+    const message = Encoder.encode("HINCRBY", key, field, increment);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  async hkeys(key) {
+    const message = Encoder.encode("HKEYS", key);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+
+  //----
+  async hset(keys) {
+    const message = Encoder.encode("HSET", keys);
+    const writeDone = await this.writeToServer(message);
+    const returnVal = await this.resolveOnData();
+
+    return returnVal;
+  }
+  //----
   destroyClient() {
     this.client.destroy();
   }
--- spec/app_spec.js
+++ spec/app_spec.js
@@ -1,8 +1,9 @@
 import CorvoNodeClient from '../corvo_node_client';
 
-describe("first test", () => {
+describe("client", () => {
+/*
   let response = "response not received";
-  const client = new CorvoNodeClient();
+  // const client = new CorvoNodeClient();
   //
   // beforeEach((done) => {
   //   client.writeToServer();
@@ -14,8 +15,66 @@ describe("first test", () => {
   // });
 
   it("write test for client set command", () => {
+  const client = new CorvoNodeClient();
     client.set("mykey", "myval").then((response) => {
       expect(response).toBe("+OK\r\n");
     });
+    client.destroyClient();
+  });
+
+  it("uses hvals command and receives an array of field values", () => {
+    const client = new CorvoNodeClient();
+    client.hset("key1", "field1", "val1").then();
+    client.hset("key1", "field2", "val2").then();
+    client.hvals("key1").then((response) => {
+      expect(response).toEqual(["val1", "val2"]);
+    });
+   client.destroyClient();
+  });
+
+  it("uses hvals command and receives an empty array for non-existent key", () => {
+    const client = new CorvoNodeClient();
+    client.hvals("key5").then((response) => {
+      expect(response).toEqual([]);
+    });
+    client.destroyClient();
+  });
+
+  it("uses strlen command and receives value of field", () => {
+    const client = new CorvoNodeClient();
+    client.hset("key1", "field1", "val1").then();
+    client.hstrlen("key1", "field1").then((response) => {
+      expect(response).toBe(4);
+    });
+    client.destroyClient();
+  });
+
+  it("uses strlen command and receives 0 when for non-existent field in key", () => {
+    const client = new CorvoNodeClient();
+    client.hset("key1", "field1", "val1").then((reponse) => {
+    });
+    client.hstrlen("key1", "field2").then((response) => {
+      expect(response).toBe(0);
+    });
+    client.destroyClient();
+  });
+*/
+});
+
+describe("client", () => {
+  let response = "response not received";
+  let client;
+  beforeEach(() => {
+    client = new CorvoNodeClient();
+  });
+  
+  it("write test for client set command", () => {
+    client.set("mykey", "myval").then((response) => {
+      expect(response).toBe("+OK\r\n");
+    });
+  });
+
+  afterEach(() => {
+    client.destroyClient();
   });
 });
