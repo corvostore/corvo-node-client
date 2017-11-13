99952879504ac1f7bf5dff777e715af9ecfd5d70
Preeti Viswanathan <vis.preeti@gmail.com>
2017-11-13 07:14:37 -0800

    add methods for commands on client

--- corvo_node_client.js
+++ corvo_node_client.js
@@ -19,26 +19,87 @@ class CorvoNodeClient {
   resolveOnData() {
     return new Promise(resolve => {
       this.client.on('data', function(data) {
-        resolve(data);
+        resolve(Decoder.decode(data));
       });
     });
   }
 
-  writeToServer(message) {
+  writeToServer(...tokens) {
     return new Promise(resolve => {
+      const message = Encoder.encode(...tokens);
       this.client.write(message, function(data) {
         resolve(1);
       });
     });
   }
 
-  async set(key, val) {
-    const message = Encoder.encode("SET", key, val);
-    const writeDone = await this.writeToServer(message);
+  async get(key) {
+    const writeDone = await this.writeToServer("GET", key);
     const returnVal = await this.resolveOnData();
-
     return returnVal;
   }
+
+  async set(key, val, ...flags) {
+    const writeDone = await this.writeToServer("SET", key, val, ...flags);
+    const returnVal = await this.resolveOnData();
+    return returnVal;
+  }
+
+  async append(key, val) {
+    const writeDone = await this.writeToServer("APPEND", key, val);
+    const returnVal = await this.resolveOnData();
+    return returnVal;
+  }
+
+  async strlen(key) {
+    const writeDone = await this.writeToServer("STRLEN", key);
+    const returnVal = await this.resolveOnData();
+    return returnVal;
+  }
+
+  async touch(...keys) {
+    const writeDone = await this.writeToServer("TOUCH", ...keys);
+    const returnVal = await this.resolveOnData();
+    return returnVal;
+  }
+
+  async incr(key) {
+    const writeDone = await this.writeToServer("INCR", key);
+    const returnVal = await this.resolveOnData();
+    return returnVal;
+  }
+
+  async decr(key) {
+    const writeDone = await this.writeToServer("DECR", key);
+    const returnVal = await this.resolveOnData();
+    return returnVal;
+  }
+
+  async exists(...keys) {
+    const writeDone = await this.writeToServer("EXISTS", ...keys);
+    const returnVal = await this.resolveOnData();
+    return returnVal;
+  }
+
+  async rename(key, newKey) {
+    const writeDone = await this.writeToServer("RENAME", key, newKey);
+    const returnVal = await this.resolveOnData();
+    return returnVal;
+  }
+
+  async renamenx(key, newKey) {
+    const writeDone = await this.writeToServer("RENAMENX", key, newKey);
+    const returnVal = await this.resolveOnData();
+    return returnVal;
+  }
+
+  async type(key) {
+    const writeDone = await this.writeToServer("TYPE", key);
+    const returnVal = await this.resolveOnData();
+    return returnVal;
+  }
+
+
 
   destroyClient() {
     this.client.destroy();
--- spec/app_spec.js
+++ spec/app_spec.js
@@ -1,21 +1,35 @@
 import CorvoNodeClient from '../corvo_node_client';
 
-describe("first test", () => {
+describe("get", () => {
   let response = "response not received";
   const client = new CorvoNodeClient();
-  //
-  // beforeEach((done) => {
-  //   client.writeToServer();
-  //   client.client.on('data', function(data) {
-  //     console.log("In testing callback");
-  //     response = data;
-  //     // done();
-  //   });
-  // });
+
+  it("returns value stored at key", () => {
+    client.set("mykey", "myval");
+    client.get("mykey").then((response) => {
+      expect(response).toBe("myval");
+      client.destroyClient();
+    });
+  });
+
+  it("returns null when key does not exist", () => {
+    client.get("mykey").then((response) => {
+      expect(response).toBe(null);
+      client.destroyClient();
+    });
+  });
+});
+
+describe("set", () => {
+  let response = "response not received";
+  const client = new CorvoNodeClient();
 
   it("write test for client set command", () => {
     client.set("mykey", "myval").then((response) => {
       expect(response).toBe("+OK\r\n");
+      client.destroyClient();
     });
   });
+
+
 });
